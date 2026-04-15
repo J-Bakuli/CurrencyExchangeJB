94de8d3c9eb35d6e20f881f521a77fc6dd18e157
J-Bakuli <julia.bakuli@yandex.ru>
2026-04-15 11:29:22 +0300

    Clarify getByCurrencyCodes parameter order

--- src/main/java/com/jb/currencyexchange/dao/ExchangeRateDao.java
+++ src/main/java/com/jb/currencyexchange/dao/ExchangeRateDao.java
@@ -5,6 +5,6 @@ import com.jb.currencyexchange.model.ExchangeRate;
 import java.util.Optional;
 
 public interface ExchangeRateDao extends BaseDao<ExchangeRate> {
-    Optional<ExchangeRate> getByCurrencyCodes(String targetCurrency, String baseCurrency);
+    Optional<ExchangeRate> getByCurrencyCodes(String baseCurrencyCode, String targetCurrencyCode);
     boolean existsByCurrencyPair(String baseCode, String targetCode);
 }
